4442d58b0c5f9d96b09729b73c1685d181456fb6
Eltone Mabodo <eltone.mabodo@gmail.com>
2024-04-10 18:33:49 +0700

    Computed Income from rice production. Will add the code for the graphs to compare organic rice income vs non-organic rice income

--- code/riceincome.R
+++ code/riceincome.R
@@ -2,3 +2,15 @@ library(tidyverse)
 
 
 #This one to be calculated from the SAMS module. Need to to dis aggregate this between organic or non-organic rice income and organic rice income
+
+data %>% 
+  select(PSAMSRiceIncome) %>% 
+  group_by(IDPoor, Gender, Ethnicity, Language) %>% 
+  summarise(AverageRiceIncome = mean(PSAMSRiceIncome),
+            MedianRiceIncome = median(PSAMSRiceIncome),
+            TotalRiceIncome = sum(PSAMSRiceIncome)) %>% 
+  ungroup()
+
+#Draw a graph to show the comparison between organic rice income and non-organic rice income
+
+ggplot()
