29f54da680bfbe9c2c258c528e7cd8928d67cfdf
MABODO Eltone <eltone.mabodo@wfp.org>
2024-04-11 14:04:17 +0700

    Added comments to the pipeline

--- code/riceincome.R
+++ code/riceincome.R
@@ -1,14 +1,18 @@
 library(tidyverse)
 
 
-#This one to be calculated from the SAMS module. Need to to dis aggregate this between organic or non-organic rice income and organic rice income
+#Using the SAMS module. Need to to dis aggregate this between organic or non-organic rice income and organic rice income
 
 data %>% 
-  select(PSAMSRiceIncome) %>% 
+  #Select the necessary variables for this analysis
+  select(PSAMSRiceIncome, IDPoor, Gender, Ethnicity, Language, Organic) %>% 
+  #Add grouping variables - Levels of dis aggregation
   group_by(IDPoor, Gender, Ethnicity, Language) %>% 
+  #Calculate the necessary descriptive statistics
   summarise(AverageRiceIncome = mean(PSAMSRiceIncome),
             MedianRiceIncome = median(PSAMSRiceIncome),
             TotalRiceIncome = sum(PSAMSRiceIncome)) %>% 
+  #Ungroup the variables - to enable further analysis
   ungroup()
 
 #Draw a graph to show the comparison between organic rice income and non-organic rice income
