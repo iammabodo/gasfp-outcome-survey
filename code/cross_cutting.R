b6d6f8d0a9cfe3cfbfa387e40a13d724538db672
MABODO Eltone <eltone.mabodo@wfp.org>
2024-04-19 17:05:31 +0700

    Cross cutting and FIES

--- code/cross_cutting.R
+++ code/cross_cutting.R
@@ -31,3 +31,76 @@ HHAsstSecurity_table_wide <- data %>%
   pivot_wider(names_from = HHAsstSecurity_lab,
               values_from = Percentage,
               values_fill =  0) 
+
+
+#CC 1.2
+
+#Load survey data
+
+data <- read_csv("Static/PROP_AAP_CRF_Sample_Survey.csv")
+
+#assign variable and value labels
+var_label(data$HHAsstAccess) <- "Have you or any member of your household been unable to access WFP assistance one or more times?"
+val_lab(data$HHAsstAccess) = num_lab("
+             0 No
+             1 Yes
+             888 Don't know
+")
+
+
+#creates a table of the weighted percentage of HHAsstAccess by
+#creating a temporary variable to display value labels 
+#and providing the option to use weights if needed
+
+
+HHAsstAccess_table_wide <- data %>% 
+  drop_na(HHAsstAccess) %>%
+  count(HHAsstAccess_lab = as.character(HHAsstAccess)) %>% # if weights are needed use instead the row below 
+  #count(HHAsstAccess_lab = as.character(HHAsstAccess), wt = nameofweightvariable)
+  mutate(Percentage = 100 * n / sum(n)) %>%
+  ungroup() %>% select(-n) %>%
+  pivot_wider(names_from = HHAsstAccess_lab,
+              values_from = Percentage,
+              values_fill =  0) 
+
+
+#CC 1.3
+#add sample data
+data <- read_csv("Static/PROP_AAP_CRF_Sample_Survey.csv")
+
+#assign variable and value labels
+var_label(data$HHAsstRespect) <- "Do you think WFPandor partner staff have treated you and members of your household respectfully?"
+var_label(data$HHDTPDign) <- "Do you think the conditions of WFP programme sites are dignified?"
+
+data <- data %>%
+  mutate(across(c(HHAsstRespect, HHDTPDign), ~labelled(., labels = c(
+    "No" = 0,
+    "Yes" = 1
+  ))))
+
+#calculate indicator and assign variable label & name
+data <- data %>% mutate(HHAsstRespectDign = case_when(
+  HHAsstRespect == 1 & HHDTPDign == 1 ~ 1,
+  TRUE ~ 0
+))
+var_label(data$HHAsstRespectDign) <- "Treated with respect while engaging in WFP programs"
+val_lab(data$HHAsstRespectDign) = num_lab("
+             0 No
+             1 Yes
+")
+
+
+#creates a table of the weighted percentage of HHAsstRespectDign by
+#creating a temporary variable to display value labels 
+#and providing the option to use weights if needed
+
+
+HHAsstRespectDign_table_wide <- data %>% 
+  drop_na(HHAsstRespectDign) %>%
+  count(HHAsstRespectDign_lab = as.character(HHAsstRespectDign)) %>% # if weights are needed use instead the row below 
+  #count(HHAsstRespectDign_lab = as.character(HHAsstRespectDign), wt = nameofweightvariable)
+  mutate(Percentage = 100 * n / sum(n)) %>%
+  ungroup() %>% select(-n) %>%
+  pivot_wider(names_from = HHAsstRespectDign_lab,
+              values_from = Percentage,
+              values_fill =  0) 
--- code/fies.R
+++ code/fies.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(RM.weights)
 
+fiesVars <- c("FIESWorried", "FIESHealthy", "FIESSkipped", "FIESRanOut", "FIESWholeDay", "FIESFewFoods", "FIESHungry", "FIESAteLess")
 #Load data and select necessary variables
 fiesData <- read_csv() %>% #Add the data file path here
   #Select necessary variables
@@ -8,17 +9,16 @@ fiesData <- read_csv() %>% #Add the data file path here
          FIESWorried, FIESHealthy, FIESFewFoods, FIESSkipped,
          FIESAteLess, FIESRanOut, FIESHungry, FIESWholeDay) %>%
   #Change the "Don't Know" and "refused to answer" responses to NAs
-  mutate(FIESWorried = case_when(FIESWorried > 1 ~ NA, T),
-         FIESHealthy = case_when(FIESHealthy > 1 ~ NA, T),
-         FIESFewFoods = case_when(FIESFewFoods > 1 ~ NA, T),
-         FIESSkipped = case_when(FIESSkipped > 1 ~ NA, T),
-         FIESAteLess = case_when(FIESAteLess > 1 ~ NA, T),
-         FIESRanOut = case_when(FIESRanOut > 1 ~ NA, T),
-         FIESHungry = case_when(FIESHungry > 1 ~ NA, T),
-         FIESWholeDay = case_when(FIESWholeDay > 1 ~ NA, T)) %>% 
+  mutate(FIESWorried = case_when(as_numeric(FIESWorried > 1 ~ NA, T)),
+         FIESHealthy = case_when(as_numeric(FIESHealthy > 1 ~ NA, T)),
+         FIESFewFoods = case_when(as_numeric(FIESFewFoods > 1 ~ NA, T)),
+         FIESSkipped = case_when(as_numeric(FIESSkipped > 1 ~ NA, T)),
+         FIESAteLess = case_when(as_numeric(FIESAteLess > 1 ~ NA, T)),
+         FIESRanOut = case_when(as_numeric(FIESRanOut > 1 ~ NA, T)),
+         FIESHungry = case_when(as_numeric(FIESHungry > 1 ~ NA, T),
+         FIESWholeDay = case_when(as_numeric(FIESWholeDay > 1 ~ NA, T)))) %>% 
   #Drop NAs from the FIES_ variables
-  drop_na(FIESWorried, FIESHealthy, FIESSkipped, FIESRanOut, FIESWholeDay,
-          FIESFewFoods, FIESHungry, FIESAteLess)
+  drop_na(any_of(FIESWorried, FIESHealthy, FIESSkipped, FIESRanOut, FIESWholeDay, FIESFewFoods, FIESHungry, FIESAteLess))
 
 ##Analyse data
 
@@ -26,14 +26,17 @@ fiesData %>%
   #Group by dissaggregation variables
   group_by(Gender, Ethnicity, Language, IDPoor) %>% 
   #Find the sum of 1's "Yes"
-  summarise(TotalWorried =  sum(FIESWorried),
-            TotalHealthy = sum(FIESHealthy),
-            TotalFewFoods = sum(FIESFewFoods),
-            TotalSkipped = summ(FIESSkipped),
-            TotalAteLess = sum(FIESAteLess),
-            TotalRanOut = sum(FIESRanOut),
-            TotalHungry = sum(FIESHungry),
-            TotalWholeDay = sum(FIESWholeDay)) 
+  summarise(TotalWorried =  sum(FIESWorried, na.rm = T),
+            TotalHealthy = sum(FIESHealthy, na.rm = T),
+            TotalFewFoods = sum(FIESFewFoods, na.rm = T),
+            TotalSkipped = summ(FIESSkipped, na.rm = T),
+            TotalAteLess = sum(FIESAteLess, na.rm = T),
+            TotalRanOut = sum(FIESRanOut, na.rm = T),
+            TotalHungry = sum(FIESHungry, na.rm = T),
+            TotalWholeDay = sum(FIESWholeDay, na.rm = T)) 
   
   
-
+numericVariables <- function(variable) {
+  mutate(variable = case_when(as.numeric(variable > 1, NA, T)))
+  variable
+}
