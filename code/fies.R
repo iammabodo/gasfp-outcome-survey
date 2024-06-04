3feb2106cf38da9040f1c4cd6e4c3ee318791d5c
MABODO Eltone <eltone.mabodo@wfp.org>
2024-06-04 17:04:56 +0700

    revised FIES and ecmen

--- code/fies.R
+++ code/fies.R
@@ -3,38 +3,24 @@ library(RM.weights)
 
 fiesVars <- c("FIESWorried", "FIESHealthy", "FIESSkipped", "FIESRanOut", "FIESWholeDay", "FIESFewFoods", "FIESHungry", "FIESAteLess")
 #Load data and select necessary variables
-fiesData <- read_csv() %>% #Add the data file path here
+fiesData <- Copy_of_Data_Format_WFP_GASFP_WO8 %>% #Add the data file path here
   #Select necessary variables
-  select(hhid, Gender, Enthinicity, Language, Gender, ACName,
-         FIESWorried, FIESHealthy, FIESFewFoods, FIESSkipped,
-         FIESAteLess, FIESRanOut, FIESHungry, FIESWholeDay) %>%
-  #Change the "Don't Know" and "refused to answer" responses to NAs
-  mutate(FIESWorried = case_when(as_numeric(FIESWorried > 1 ~ NA, T)),
-         FIESHealthy = case_when(as_numeric(FIESHealthy > 1 ~ NA, T)),
-         FIESFewFoods = case_when(as_numeric(FIESFewFoods > 1 ~ NA, T)),
-         FIESSkipped = case_when(as_numeric(FIESSkipped > 1 ~ NA, T)),
-         FIESAteLess = case_when(as_numeric(FIESAteLess > 1 ~ NA, T)),
-         FIESRanOut = case_when(as_numeric(FIESRanOut > 1 ~ NA, T)),
-         FIESHungry = case_when(as_numeric(FIESHungry > 1 ~ NA, T),
-         FIESWholeDay = case_when(as_numeric(FIESWholeDay > 1 ~ NA, T)))) %>% 
-  #Drop NAs from the FIES_ variables
-  drop_na(any_of(FIESWorried, FIESHealthy, FIESSkipped, FIESRanOut, FIESWholeDay, FIESFewFoods, FIESHungry, FIESAteLess))
+  select(ADMIN4Name, ACName, HHID, HHList, HHBaseline,HHHEthnicity, HHHLanguage,
+         IDPoor, HHHSex, starts_with("FIES"))
 
 ##Analyse data
 
 fiesData %>% 
-  #Group by dissaggregation variables
-  group_by(Gender, Ethnicity, Language, IDPoor) %>% 
-  #Find the sum of 1's "Yes"
-  summarise(TotalWorried =  sum(FIESWorried, na.rm = T),
-            TotalHealthy = sum(FIESHealthy, na.rm = T),
-            TotalFewFoods = sum(FIESFewFoods, na.rm = T),
-            TotalSkipped = summ(FIESSkipped, na.rm = T),
-            TotalAteLess = sum(FIESAteLess, na.rm = T),
-            TotalRanOut = sum(FIESRanOut, na.rm = T),
-            TotalHungry = sum(FIESHungry, na.rm = T),
-            TotalWholeDay = sum(FIESWholeDay, na.rm = T)) 
-  
+  #Find the mean of 1's "Yes"
+  summarise(TotalWorried =  mean(FIES_Worried, na.rm = T),
+            TotalHealthy = mean(FIES_Healthy, na.rm = T),
+            TotalFewFoods = mean(FIES_FewFoods, na.rm = T),
+            TotalSkipped = mean(FIES_Skipped, na.rm = T),
+            TotalAteLess = mean(FIES_AteLess, na.rm = T),
+            TotalRanOut = mean(FIES_RanOut, na.rm = T),
+            TotalHungry = mean(FIES_Hungry, na.rm = T),
+            TotalWholeDay = mean(FIES_WholeDay, na.rm = T)) 
+ 
   
 numericVariables <- function(variable) {
   mutate(variable = case_when(as.numeric(variable > 1, NA, T)))
