b8ac4b32814d732f538d9822df7d7b2b61e66fea
MABODO Eltone <eltone.mabodo@wfp.org>
2024-06-06 16:18:39 +0700

    Imported all relevant rosters to measure the level of agricultural productivity. Will have to merge these three datasets though. These rosters will be used to calculate income from rice production, percentage of smallholder farmers reporting increases in the production rice as well as the average post harvest losses indicators.

--- code/agriculture_productivity.R
+++ code/agriculture_productivity.R
@@ -1,101 +1,49 @@
+# This module is used to calculate three indicators, i.e., Percentage of farmers reporting increase in the production of rice,
+# average post harvest losses and the total household income from rice production. These indicators will be calculated only for 
+# the farmers who have grown rice (any type) in the last 12 months.
+
+# Loading required libraries
 library(tidyverse)
 library(labelled)
 library(expss)
+library(readxl)
 
-#add sample data
-data <- read_csv("~/GitHub/RAMResourcesScripts/Static/SAMS_CRF_30_PHL_Sample_Survey/SAMS_module_Indicator30_submodule_RepeatSAMSPHL.csv")
-
-
-##SAMS 30 - Post Harvest Losses
-
-
-#can only download repeat csv data as zip file from moda with group names - will update this code to remove group names
-#rename to remove group names
-data <- data %>% rename(PSAMSPHLCommName = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommName')
-data <- data %>% rename(PSAMSPHLCommName_oth = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommName_oth')
-data <- data %>% rename(PSAMSPHLCommClass = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommClass')
-data <- data %>% rename(PSAMSPHLCommQntHand = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommQntHand')
-data <- data %>% rename(PSAMSPHLCommQntHandUnit = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommQntHandUnit')
-data <- data %>% rename(PSAMSPHLCommQntHandUnit_oth = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommQntHandUnit_oth')
-data <- data %>% rename(PSAMSPHLCommQntLost = 'SAMS_module/Indicator30_submodule/RepeatSAMSPHL/PSAMSPHLCommQntLost')
-#also rename the _parent_index variable to farmer number 
-data <- data %>% rename(PSAMSPHLFarmerNum = '_parent_index')
-
-#assign variable and value labels
-var_label(data$PSAMSPHLCommName) <- "What is the name of commodity?"
-var_label(data$PSAMSPHLCommClass) <- "Which of the following groups does this commodity belong to?"
-var_label(data$PSAMSPHLCommQntHand) <- "What is the amount of this commodity initially stored?"
-var_label(data$PSAMSPHLCommQntHandUnit) <- "Enter unit of measure."
-var_label(data$PSAMSPHLCommQntLost) <- "Of the total quantity you stored how much was lost?"
-
-#Calculate % loss per row
-data <- data %>% mutate(perc_loss = round((PSAMSPHLCommQntLost /(PSAMSPHLCommQntHand) * 100),1))
-#Average loss per farmer 
-avglossperfarmer_table <- data %>% group_by(PSAMSPHLFarmerNum) %>% summarise(avglossperfarmer = mean(perc_loss))
-#Average across farmers
-average_phl_loss_table <- avglossperfarmer_table %>% summarise(average_phl_loss = mean(avglossperfarmer))
+# Import first roster data
+SAMSRoster1 <- read_excel("data/Copy of Data_Format_WFP_GASFP_WO8.xlsx", 
+    sheet = "Roster_PSAMSRice") %>% 
+  # Selecting required columns
+  select(interview__key, interview__id, Roster_PSAMSRice__id, 
+         PSAMSRiceHarvestsNmb, PSAMSNutCropIncr, PSAMSPHLCommEnough,
+         PSAMSRiceInputsMN, PSAMSRiceSell, PSAMSRiceSellTime, PSAMSRiceSellQuant,
+         PSAMSRiceSellMN, PSAMSRiceRevenue, PSAMSRiceIncome, Income)
 
 
-##SAMS 31 - Increased Production
+# Import second roster data
+SAMSRoster2 <- read_excel("data/Copy of Data_Format_WFP_GASFP_WO8.xlsx", 
+    sheet = "Roster_Q15_21HarvestNmb") %>% 
+  # Selecting required columns
+  select(interview__key, interview__id, Roster_PSAMSRice__id, Roster_HarvestNmb__id, 
+         PSAMSPHLCommArea, PSAMSPHLCommArea_Unit, PSAMSPHLCommArea_Unit_OTH, PSAMSPHLCommQuant,
+         PSAMSPHLCommQntHand, PSAMSPHLCommQntLost)
 
-#can only download repeat csv data as zip file from moda with group names - will update this code to remove group names
-#rename to remove group names
-data <- data %>% rename(PSAMSNutCropName = 'SAMS_module/Indicator31_submodule/RepeatNutCrop/PSAMSNutCropName')
-data <- data %>% rename(PSAMSNutCropName_oth = 'SAMS_module/Indicator31_submodule/RepeatNutCrop/PSAMSNutCropName_oth')
-data <- data %>% rename(PSAMSNutCropQuant = 'SAMS_module/Indicator31_submodule/RepeatNutCrop/PSAMSNutCropQuant')
-data <- data %>% rename(PSAMSNutCropQuantUnit = 'SAMS_module/Indicator31_submodule/RepeatNutCrop/PSAMSNutCropQuantUnit')
-data <- data %>% rename(PSAMSNutCropIncr = 'SAMS_module/Indicator31_submodule/RepeatNutCrop/PSAMSNutCropIncr')
-data <- data %>% rename(index = '_parent_index')
+# Import the data with other relevant variables from the household data set
+HHLevelData <- read_excel("data/Copy of Data_Format_WFP_GASFP_WO8.xlsx") %>% 
+  # Selecting required columns
+  select(interview__key, interview__id, HHID, ADMIN4Name, ACName, HHBaseline, HHList, HHHSex,
+         HHHEducation, HHHEthnicity, HHHLanguage, IDPoor, HHIncTot, contains("SAMSPHL")) %>% 
+  # Pivot longer using PSAMSPHLCommN_1 and PSAMSPHLCommN_2 variables
+  pivot_longer(cols = c("PSAMSPHLCommN__1", "PSAMSPHLCommN__2"), 
+               names_to = "RiceType", 
+               values_to = "Produced") %>%
+  # Mutate the RiceType variable to be more descriptive
+  mutate(RiceType = case_when(RiceType == "PSAMSPHLCommN__1" ~ "Organic Rice",
+                              RiceType == "PSAMSPHLCommN__2" ~ "Non Organic Rice")) %>%
+  # Mutate the Produced variable to be more descriptive
+  mutate(Produced = case_when(Produced == 1 ~ "Yes",
+                              TRUE ~ "No")) %>%
+  # Filter out the rows where the Produced variable is "Yes"
+  filter(Produced == "Yes")
 
-data2 <- data2 %>% rename(RespSex = 'Demographic_module/DemographicBasic_submodule/RespSex')
-data2 <- data2 %>% rename(index = '_index')
-
-#assign variable and value labels
-var_label(data2$RespSex) <- "Sex of the Respondent"
-var_label(data$PSAMSNutCropName) <- "What is the name of crop?"
-var_label(data$PSAMSNutCropQuant) <- "How much of this commodity did you produce in the last 12 months?"
-var_label(data$PSAMSNutCropQuantUnit) <- "Enter unit of quantity produced"
-var_label(data$PSAMSNutCropIncr) <- "Did you produce more, less or the same amount of this nutritious crop in the last 12 months compared to the 12 months before that?"
+# Join the HHLevelData with the first and second roster data using the interview__key variable
 
 
-val_lab(data2$RespSex) = num_lab("
-             0 Female
-             1 Male
-")
-val_lab(data$PSAMSNutCropName) = num_lab("
-             1 Crop 1 
-             2 Crop 2
-             3 Crop 3
-             4 Crop 4
-             5 Crop 5
-             999 Other
-")
-val_lab(data$PSAMSNutCropIncr) = num_lab("
-             1 More
-             2 Less
-             3 The same
-             9999 Not applicable
-")
-
-#join dataset "data" & "data2"
-data <- data %>% left_join(data2, by = "index")
-
-#selecting farmers that grew "Crop 1" show proportion reporting an increase, decrease or the same amount of production as the year before
-SAMS31_table_total_wide <- data %>% filter(PSAMSNutCropName == 1) %>% filter(PSAMSNutCropIncr != 9999) %>%
-  drop_na(PSAMSNutCropIncr) %>%
-  count(PSAMSNutCropIncr_lab = as.character(PSAMSNutCropIncr)) %>% # if weights are needed use instead the row below 
-  mutate(Percentage = 100 * n / sum(n)) %>%
-  ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = PSAMSNutCropIncr_lab,
-              values_from = Percentage,
-              values_fill =  0) 
-
-#selecting farmers that grew "Crop 1" show proportion reporting an increase, decrease or the same amount of production as the year before
-SAMS31_table_bysex_wide <- data %>% filter(PSAMSNutCropName == 1) %>% filter(PSAMSNutCropIncr != 9999) %>% group_by(RespSex_lab = as.character(RespSex)) %>%
-  drop_na(PSAMSNutCropIncr) %>%
-  count(PSAMSNutCropIncr_lab = as.character(PSAMSNutCropIncr)) %>% # if weights are needed use instead the row below 
-  mutate(Percentage = 100 * n / sum(n)) %>%
-  ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = PSAMSNutCropIncr_lab,
-              values_from = Percentage,
-              values_fill =  0) 
