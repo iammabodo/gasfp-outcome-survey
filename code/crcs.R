ee14d59411ceda95d805b28a0f50482696017ad5
MABODO Eltone <eltone.mabodo@wfp.org>
2024-06-19 10:56:08 +0700

    Included different resilience capacities for the CRCS indicators

--- code/crcs.R
+++ code/crcs.R
@@ -4,8 +4,7 @@ library(tidyverse)
 
 # Load the data set
 
-CRCSData <- read_excel("data/WFP_GASFP_WO8_Cleaned_Numeric.xlsx",
-                       sheet = 1) %>%  #Input file path here
+CRCSData <- read_excel("data/WFP_GASFP_WO8_Cleaned_Numeric.xlsx") %>%  #Input file path here
   #Select the necessary variables for this analysis - dis aggregation modules and $PSAMSRiceIncome variable
   select(HHID, SEX_HHH, HHHEthnicity, HHHLanguage, starts_with("HHCRCS"), -c(HHCRCSShocks, HHCRCSFloods, HHCRCSWildFire,
                                                                              HHCRCSHeatWave, HHCRCSStorms, HHCRCSDroughts)) %>%
@@ -32,8 +31,61 @@ CRCSData <- read_excel("data/WFP_GASFP_WO8_Cleaned_Numeric.xlsx",
   mutate(CRCSCategory = case_when(
     CRCSScore < 33 ~ "Low",
     CRCSScore >= 33 & CRCSScore < 66 ~ "Medium",
-    CRCSScore >= 66 ~ "High")) #%>% 
-  #find_outliers() # Remove outliers by uncommenting this line
+    CRCSScore >= 66 ~ "High")) %>% 
+  # Calculate Anticipatory Capacity
+  mutate(AnticipatoryCapacity = (HHCRCSPrepared - 1)/4) %>% 
+  mutate(AnticipatoryCapacity = AnticipatoryCapacity * 100) %>%
+  # Calculate Absorptive Capacity
+  mutate(AbsorptiveCapacity = (HHCRCSBounceBack - 1)/4) %>%
+  mutate(AbsorptiveCapacity = AbsorptiveCapacity * 100) %>% 
+  # Calculate Transformative Capacity
+  mutate(TransformativeCapacity = (HHCRCSIncSrcChange - 1)/4) %>%
+  mutate(TransformativeCapacity = TransformativeCapacity * 100) %>%
+  # Calcualate Adaptive Capacity
+  mutate(AdaptiveCapacity = (HHCRCSGetBy - 1)/4) %>%
+  mutate(AdaptiveCapacity = AdaptiveCapacity * 100) %>% 
+  # Calculate the categorised capacities for AnticipatoryCapacity, AbsorptiveCapacity, TransformativeCapacity and AdaptiveCapacity
+  mutate(AnticipatoryCapacityCategory = case_when(
+    AnticipatoryCapacity < 33 ~ "Low",
+    AnticipatoryCapacity >= 33 & AnticipatoryCapacity < 66 ~ "Medium",
+    AnticipatoryCapacity >= 66 ~ "High")) %>%
+  mutate(AbsorptiveCapacityCategory = case_when(
+    AbsorptiveCapacity < 33 ~ "Low",
+    AbsorptiveCapacity >= 33 & AbsorptiveCapacity < 66 ~ "Medium",
+    AbsorptiveCapacity >= 66 ~ "High")) %>%
+  mutate(TransformativeCapacityCategory = case_when(
+    TransformativeCapacity < 33 ~ "Low",
+    TransformativeCapacity >= 33 & TransformativeCapacity < 66 ~ "Medium",
+    TransformativeCapacity >= 66 ~ "High")) %>%
+  mutate(AdaptiveCapacityCategory = case_when(
+    AdaptiveCapacity < 33 ~ "Low",
+    AdaptiveCapacity >= 33 & AdaptiveCapacity < 66 ~ "Medium",
+    AdaptiveCapacity >= 66 ~ "High"))
+
+
+
+# Calculate percentage of households in different Resilience Capacities
+CRCSData %>% 
+  group_by(AnticipatoryCapacityCategory) %>% 
+  summarise(Count = n()) %>% 
+  mutate(Percentage = (Count / sum(Count)) * 100)
+
+CRCSData %>% 
+  group_by(AbsorptiveCapacityCategory) %>% 
+  summarise(Count = n()) %>% 
+  mutate(Percentage = (Count / sum(Count)) * 100)
+
+
+CRCSData %>%
+  group_by(TransformativeCapacityCategory) %>% 
+  summarise(Count = n()) %>% 
+  mutate(Percentage = (Count / sum(Count)) * 100)
+
+CRCSData %>% 
+  group_by(AdaptiveCapacityCategory) %>% 
+  summarise(Count = n()) %>% 
+  mutate(Percentage = (Count / sum(Count)) * 100)
+
 
 
 CRCSData %>% 
